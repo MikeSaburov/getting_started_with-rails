c815e0ab192bedff6937fc7356d60559193c0d62
Mikhail Saburov <saburov1985@gmail.com>
2026-07-29 21:38:00 +1100

    Создание товара Добавление формы для создания товара

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,25 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id]) #получаем товар по id
   end
+
+  # 1. Этот метод срабатывает, когда мы переходим по кнопке "Добавить новый товар"
+  def new
+    @product = Product.new # Создаем пустой чертеж товара для будущей формы
+  end
+
+  def create
+    # Применяем Strong Parameters (нашу защиту от хакеров)
+    product_params = params.require(:product).permit(:name)
+    
+    # Создаем товар с безопасными параметрами
+    @product = Product.new(product_params)
+
+    # Пытаемся сохранить в базу данных
+    if @product.save
+      redirect_to products_path # Если всё ок — возвращаем пользователя на главную
+    else
+      # Если сработала валидация (имя пустое) — заново рисуем страницу формы (new.html.erb)
+      render :new, status: :unprocessable_entity 
+    end
+  end
 end
